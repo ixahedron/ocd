5d8a8248a8c61c44c53f478a6dbe694d48885acc
Ilona <ixahedron@users.noreply.github.com>
2017-11-04 01:50:22 +0100

    Add (unnecessary) code

--- 3_3.hs
+++ 3_3.hs
@@ -20,7 +20,7 @@ Prelude> gcd 1021763679 519424709
 So we're good to go
 
 *Main> let (u,v) = euc e1 e2              
-*Main> ((nexp c1 u) * (nexp c2 v)) `mod` n
+*Main> (nexp c1 u) * (nexp c2 v) `mod` n
 1054592380
 
 *Main> let m = it
@@ -31,6 +31,14 @@ So we're good to go
 
 Checks out, good
 
+Let's even write up a line of code for this equation, whatever
 -}
+
+usingTwoExponentsWasAMistake :: Integer -> (Integer,Integer) -> (Integer,Integer) -> Integer
+usingTwoExponentsWasAMistake n (e1,e2) (c1,c2) = if gcd e1 e2 == 1
+                                                 then (nexp c1 u) * (nexp c2 v) `mod` n
+                                                 else error "gcd e1 e2 /= 1, can't decrypt"
+  where nexp = mexp n
+        (u,v) = euc e1 e2
 
 -- wow, that was a quickly done homework
