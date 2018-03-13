f0918103722e1602383f578765922f994d622d92
Ilona <ixahedron@users.noreply.github.com>
2018-03-13 20:56:23 +0200

    Add Sect.6.3

---
+++ src/Chapter06/Sect3.hs
@@ -0,0 +1,181 @@
+import EllipticCurve hiding (dadd)
+import Lib (eGCD)
+import Data.List (find, group)
+
+-- 6.8
+
+{-
+
+λ> ecdlpTrialError (ELPF e (4,2)) (ELPF e (0,1))
+5
+
+-}
+
+--ecdlp p q = n <=> q = np
+ecdlpTrialError :: ELPF -> ELPF -> Integer
+ecdlpTrialError p q = head $ [n | n<-[1..], q == n *^ p]
+
+-- 6.9
+
+{-
+
+Allright, let's write:
+  n = is+r, i∈ℤ, s>0, 0<=r<s.
+
+Then
+  Q = (is+r)P = isP + rP = O + rP = rP
+
+If r = n0, we're done. But suppose r > n0. Then let's examine Q-Q:
+
+  O = Q - Q = rP - n0 * P = (r - n0)P
+
+So r-n0 > s, right? But s must be the smallest solution to kP = O,
+so contradiction. So n0 = r really.
+QED
+
+-}
+
+-- 6.10
+
+{-
+
+We want to solve Q = nP.
+Using the Basis Problem algo, express both points as:
+
+  Q = k1*B1 + k2*B2
+  P = l1*B1 + l2*B2
+
+and substitute in:
+
+  Q = nP
+  k1*B1 + k2*B2 = n(l1*B1 + l2*B2)
+  (n*l1-k1)B1 + (n*l2-k2)B2 = O
+
+Hmmmmm... {B1,B2} is a basis, of course, so if we have m1*B1 + m2*B2 = O, it must
+be that m1 = 0 and m2 = 0, since otherwise, B1 and B2 would be linearly dependent (right?)
+Next, we solve:
+
+  n*l1-k1 = 0
+
+From the Basis algo we know both k1 and l1, so we just take n = k1 * l1^(-1) in F_p
+and we've solved the problem.
+
+-}
+
+-- 6.11
+
+{-
+
+λ> ex611 23 13 83 (24,14) 19
+EP (24,69)
+λ> ex611 143 367 613 (195,9) 23
+EP (485,573)
+λ> ex611 1828 1675 1999 (1756,348) 11
+EP (1068,1540)
+λ> ex611 1541 1335 3221 (2898,439) 3211
+EP (243,1875)
+
+-}
+
+-- Double-and-add algo for elliptic curves
+dadd :: ELPF -> Integer -> ELPF
+dadd p n = dadd_aux n p O
+  where dadd_aux 0 _ r = r
+        dadd_aux n q r | odd n = dadd_aux (n `div` 2) (q +^ q) (r +^ q)
+                       | otherwise = dadd_aux (n `div` 2) (q +^ q) r
+
+ex611 a b p (x,y) n = let e = ElCurveF a b p; c = ELPF e (x,y) in putStrLn . showReadable $ dadd c n
+
+-- 6.12
+
+{-
+
+λ> mapM_ compareExps [349,9337,38728,8379483273489]
+bin: 6; tern: 5
+bin: 7; tern: 5
+bin: 7; tern: 6
+bin: 21; tern: 11
+
+-}
+
+binExpansion :: Integer -> [Integer]
+binExpansion 0 = [0]
+binExpansion n = (n `mod` 2) : (binExpansion $ n `div` 2)
+
+ternExpansion :: Integer -> [Integer]
+ternExpansion = concat . replaceRun . shift . group . binExpansion
+  where replaceRun [] = []
+        replaceRun ([]:xs) = replaceRun xs
+        replaceRun (zs@(0:_):xs) = zs:(replaceRun xs)
+        replaceRun ([1,0]:xs) = [1,0]:(replaceRun xs)
+        replaceRun (run@(1:_):xs) = let rp = (-1):(replicate ((length run)-2) 0)++[1] in rp:(replaceRun xs)
+                
+        shift [] = []
+        shift zs@((0:_):[]) = zs
+        shift (zs@(0:_):xs) = zs:shift xs
+        shift (ones@(1:_):[0]:[]) = (ones++[0]):[]
+        shift (ones@(1:_):(0:zs):xs) = (ones++[0]):zs:(shift xs)
+
+compareExps n = let bin = length $ filter (/= 0) (binExpansion n);
+                    tern = length $ filter (/= 0) (ternExpansion n)
+                in putStrLn $ "bin: " ++ show bin ++ "; tern: " ++ show tern
+
+
+-- 6.13
+
+{-
+
+Oh, we also need a number N = |E|.
+
+Again, what we need is to find such an n that Q = nP. For Pollard's method, we also
+need to find an appropriate f for mixing up elements. Let's snatch everything from
+Subsection 5.5.2, why not?
+
+f(L) = { P+L if 0 <= x_L < p/3
+       { 2L  if p/3 <= x_L < 2p/3
+       { Q+L if 2p/3 <= x_L < p
+
+So our function values for i steps and 2i steps will look like
+  f_i(L) = α_i*P + β_i*Q, f_2i(L) = γ_i*P + δ_i*Q,
+
+where we know the values of α, β, γ, δ (computed modulo N).
+
+If we find a match (again, after approx. √N steps), then
+
+  α_i*P + β_i*Q = γ_i*P + δ_i*Q <=> (α_i-γ_i)*P = (δ_i-β_i)*Q
+
+If gcd(N, δ_i-β_i)=1, we're in incredible luck and multiply both sides by
+(δ_i-β_i)^(-1) to solve the problem. Otherwise we do the same procedure as in normal Pollard's
+algo, where we divide by that gcd, solve the resulting equation and then check each of gcd's
+possible solutions.
+
+-}
+
+data PollardExps = PExp {α :: Integer, β :: Integer, γ :: Integer, δ :: Integer}
+  deriving (Show)
+
+ecdlpOnRhoMatch :: ELPF -> ELPF -> Integer -> PollardExps -> Maybe Integer
+ecdlpOnRhoMatch q1@ELPF{..} q2 n PExp{..}= find (\l -> l *^ q1 == q2) lhs
+  where norm x = if x < 0 then x+n else x
+        a = norm $ α-γ; b = norm $ δ-β;
+        (s,_,d) = eGCD b n; w = ((a * s) `mod` n) `div` d
+        lhs = [w + k*(n `div` d) | k<-[0..d-1]]
+
+--ecdlpPollardRho P Q = n <=> Q = nP
+ecdlpPollardRho :: ELPF -> ELPF -> Maybe Integer
+ecdlpPollardRho q1 q2 = let (y,γ,δ) = fRho (q1,1,0);
+                        in ecdlpOnRhoMatch q1 q2 n $ rhoxps q1 y $ PExp 1 0 γ δ
+  where rhoxps x y exps | x == y = exps
+        rhoxps x y PExp{..} = let (x',α',β') = fRho (x,α,β);
+                                  (y',γ',δ') = fRho . fRho $ (y,γ,δ)
+                              in rhoxps x' y' $ PExp α' β' γ' δ'
+                              
+        n = curveSize . curve_ $ q1
+        
+        fRho :: (ELPF, Integer, Integer) -> (ELPF, Integer, Integer)
+        fRho (q,α,β) | p'/3 > x' && x' >= 0 = (q +^ q1, (α+1) `mod` n, β)
+                     | 2*p'/3 > x' && x' >= p'/3 = (2 *^ q, (2*α) `mod` n, (2*β) `mod` n)
+                     | p' > x' && x' >= 2*p'/3 = (q +^ q2, α, (β+1) `mod` n)
+          where x' = if q == O then 0 else fromInteger . fst . q_ $ q;
+                p' = fromInteger . p_ . curve_ $ q1
+
---
+++ src/EllipticCurve.hs
@@ -0,0 +1,72 @@
+module EllipticCurve where
+
+import Lib (inv, jacobi, (≡))
+
+data ElCurveF = ElCurveF {a_ :: Integer, b_ :: Integer, p_ :: Integer}
+  deriving (Show, Eq)
+
+data ELPF = O | ELPF {curve_ :: ElCurveF, q_ :: (Integer, Integer)}
+  deriving (Show, Eq)
+
+class AdditiveGroup p where
+  -- identity
+  zero :: p
+  (+^) :: p -> p -> p
+  (^+) :: p -> p -> p
+  (^+) = (+^)
+  -- inverse
+  negateP :: p -> p
+  -- subtraction
+  (-^) :: p -> p -> p
+  p -^ p' = p +^ negateP p'
+  -- scalar multiplication
+  (*^) :: (Integral a) => a -> p -> p
+  (^*) :: (Integral a) => p -> a -> p
+  p ^* k = k *^ p
+
+instance AdditiveGroup ELPF where
+  zero = O
+  negateP O = O
+  negateP (ELPF c@ElCurveF{..} (x,y)) = ELPF c (x,p_-y)
+
+  (*^) _ O = O
+  (*^) 0 p = O
+  (*^) 1 p = p
+  (*^) k p = dadd p k
+
+  O +^ p2 = p2
+  p1 +^ O = p1
+  p1 +^ p2 | (not . onCurve $ p1) || (not . onCurve $ p2) = error "points not on the curve"
+           | p1 == negateP p2 = O
+           | negateP p1 == p2 = O
+  (ELPF c1 (x1,y1)) +^ (ELPF c2@ElCurveF{..} (x2,y2))
+    | c1 /= c2 = error "not the same curve"
+    | otherwise = let λ = if (x1,y1) == (x2,y2) then (3*x1*x1+a_) * inv (2*y1) p_
+                                                else (y2-y1) * inv (x2-x1) p_;
+                      x3 = (λ^2-x1-x2) `mod` p_; y3 = (λ*(x1-x3)-y1) `mod` p_;
+                  in ELPF ElCurveF{..} (x3,y3)
+
+-- Double-and-add algo for elliptic curves
+dadd :: Integral a => ELPF -> a -> ELPF
+dadd p n = dadd_aux n p O
+  where dadd_aux 0 _ r = r
+        dadd_aux n q r | odd n = dadd_aux (n `div` 2) (q +^ q) (r +^ q)
+                       | otherwise = dadd_aux (n `div` 2) (q +^ q) r
+
+onCurve :: ELPF -> Bool
+onCurve O = True
+onCurve (ELPF ElCurveF{..} (x,y)) = y^2 ≡ (x^3 + a_*x + b_) $ p_ 
+
+showReadable :: ELPF -> String
+showReadable ELPF{..} = "EP " ++ show q_
+
+curveSize :: Integral a => ElCurveF -> a
+curveSize = fromIntegral . length . listPoints
+
+listPoints :: ElCurveF -> [ELPF]
+listPoints e = O:(map (ELPF e) $ listPointsReadable e)
+
+listPointsReadable :: ElCurveF -> [(Integer,Integer)]
+listPointsReadable e@ElCurveF{..} = concat . map points $ [0..p_-1]
+  where points x | jacobi (x^3 + a_*x + b_) p_ == -1 = []
+                 | otherwise = [(x,y) | y<-[0..p_-1], onCurve $ ELPF e (x,y)]
